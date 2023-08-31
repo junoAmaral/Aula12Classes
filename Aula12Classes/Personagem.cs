b2ed3d52a4a334836c0a2960f518e24db859b0af
Juno <juno.estudos@outlook.com>
2023-08-31 16:34:39 -0300

    Add files via upload

---
+++ Aula12Classes/Personagem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula12Classes
+{
+    internal class Personagem
+    {
+        //ATRIBUTOS (VARIÁVEIS):
+        public string nome;
+        public string raca;
+        public int poder;
+
+        //MÉTODOS (FUNÇÕES):
+        public void mostra()
+        {
+            Console.WriteLine($"Personagem {nome}, um {raca} com poder de {poder}.");
+        }
+    }
+}
---
+++ Aula12Classes/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using Aula12Classes;
+
+Console.WriteLine("Olá, nerd! Bem-vinde ao RPG da Programação. Escolha seu personagem:");
+
+Personagem bardo = new Personagem();
+bardo.nome = "Dudu";
+bardo.raca = "Humano";
+bardo.poder = 1000;
+
+bardo.mostra();
+
+Personagem mago = new Personagem();
+mago.nome = "Du";
+mago.raca = "Elfo negro";
+mago.poder = 1500;
+
+mago.mostra();
+
+Personagem bruxo = mago;
+bruxo.nome = "Matheus";
+bruxo.mostra();
+
+Personagem guerreiro = new Personagem();
+guerreiro.nome = "Conradito";
+guerreiro.raca = "Anão";
+guerreiro.poder = 500;
+
+guerreiro.mostra();
